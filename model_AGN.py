dev: token-level; softmax fix softmax in metrics

model_AGN.py
@@ -101,11 +101,12 @@ class AGN(nn.Module):
 
         self.dropout = nn.Dropout(self.dropout_rate)
 
-    def forward(self, x, gi):
+    def forward(self, x, gi):  # x: bert output; gi: statistical feature
         softmax_output = F.softmax(x, dim=-1)
-        valve, _ = torch.max(softmax_output, dim=-1)
-        valve_mask = (valve < self.valve_rate)
-        valve_mask_expanded = valve_mask.unsqueeze(-1)
+        valve, _ = torch.max(softmax_output, dim=-1)  # The max value in each token label vector
+
+        valve_mask = (valve < self.valve_rate)  # [batch_size, num_token]
+        valve_mask_expanded = valve_mask.unsqueeze(-1)  # [batch_size, num_token, num_label]
         valve_mask_expanded = valve_mask_expanded.expand(-1, -1, 9)
 
         sf = gi * valve_mask_expanded
@@ -120,15 +121,15 @@ class AGNModel(nn.Module):
         self.config = config
         self.task = config["task"]
 
-        # 加载预训练的 BERT 模型
+        # pretrained bert
         self.bert = BertModel.from_pretrained(config['pretrained_model_dir'])
         bert_output_feature_size = self.bert.config.hidden_size
 
-        # GI 层
+        # GI (statistical feature)
         self.gi_linear = nn.Linear(self.config["ae_latent_dim"], bert_output_feature_size)
         self.gi_dropout = nn.Dropout(self.config.get('dropout_rate', 0.1))
 
-        # AGN 层
+        # AGN (valve gate)
         self.agn = AGN(feature_size=9,
                        dropout_rate=0.1,
                        valve_rate=self.config.get('valve_rate', 0.3),

ner_dataloader.py
@@ -39,28 +39,6 @@ class NerDataset(Dataset):
         return data
 
 
-def collate_fn(batch):
-    batch_token_ids = [item['token_ids'] for item in batch]
-    batch_segment_ids = [item['segment_ids'] for item in batch]
-    batch_sf = [item['sf_vector'] for item in batch]
-    batch_label_ids = [item['label_ids'] for item in batch]
-
-    batch_token_ids_padded = torch.nn.utils.rnn.pad_sequence(batch_token_ids, batch_first=True, padding_value=0)
-    attention_masks = torch.nn.utils.rnn.pad_sequence([torch.ones_like(ids) for ids in batch_token_ids],
-                                                      batch_first=True, padding_value=0)
-    batch_segment_ids_padded = torch.nn.utils.rnn.pad_sequence(batch_segment_ids, batch_first=True, padding_value=0)
-    batch_sf_padded = torch.nn.utils.rnn.pad_sequence(batch_sf, batch_first=True, padding_value=0)
-    batch_label_ids_padded = torch.nn.utils.rnn.pad_sequence(batch_label_ids, batch_first=True, padding_value=-100)
-
-    return {
-        'token_ids': batch_token_ids_padded,
-        'segment_ids': batch_segment_ids_padded,
-        'sf_vector':  batch_sf_padded,
-        'attention_mask': attention_masks,
-        'label_ids': batch_label_ids_padded
-    }
-
-
 def generate_token_tcol_vectors(all_tokens, all_labels):
     # 提取所有可能的标签
     possible_labels = set(label for labels in all_labels for label in labels if
@@ -205,8 +183,6 @@ class NerDataLoader:
                 self.autoencoder.trainEncoder(data=token_sfs, batch_size=self.batch_size, epochs=self.ae_epochs, device=self.device)
             token_sfs = self.autoencoder.predict(token_sfs, batch_size=self.batch_size, device=self.device)
 
-
-        # decomposite
         token_sf_dict = {}
         for i, token_id in enumerate(token_ids):
             token_sf_dict[token_id] = token_sfs[i]
@@ -215,22 +191,21 @@ class NerDataLoader:
             token_ids = sentence['token_ids']
             sentence_sf = []
             for token_id in token_ids:
-                if token_id not in [101, 102]:
+                if token_id in [101, 102]:
+                    sentence_sf.append([0] * 9)
+                else:
                     token_sf_vector = token_sf_dict.get(token_id)
                     sentence_sf.append(token_sf_vector.tolist())
-                else:
-                    sentence_sf.append([0] * 9)
             sentence["sf_vector"] = sentence_sf
+
         return data
 
     def _read_data(self, file_path, is_train=False):
         dataset = read_json(file_path)
         data = []
-        tfidf_corpus = []
         all_label_set = set()
         for obj in dataset:
             raw_text = ' '.join(obj['tokens'])
-            tfidf_corpus.append(raw_text)
             first, last = None, None
             token_ids, tag_ids = [], []
             for i, (tag, token) in enumerate(zip(obj['ner_tags'], obj['tokens'])):
@@ -243,7 +218,6 @@ class NerDataLoader:
             token_ids = [first] + token_ids[:self.max_len - 2] + [last]
             tag_ids = [-100] + tag_ids[:self.max_len - 2] + [-100]
             assert len(token_ids) == len(tag_ids)
-
             data.append({
                 'raw_text': raw_text,
                 'token_ids': token_ids,

ner_metrics.py
@@ -1,10 +1,10 @@
 import logging
-import os
 from collections import defaultdict
 import torch
 from sklearn.preprocessing import MultiLabelBinarizer
 from utils import move_to_device
 from seqeval.metrics import accuracy_score, precision_score, recall_score, f1_score
+import torch.nn.functional as F
 
 
 class NerMetrics:
@@ -22,10 +22,6 @@ class NerMetrics:
         self.stopped_epoch = 0
         self.epochs = config["epochs"]
 
-        labels = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        self.mlb = MultiLabelBinarizer(classes=labels)
-        self.mlb.fit(labels)  # 确保所有可能的标签都被考虑
-
         self.label2id = {'O': 0, 'B-PER': 1, 'I-PER': 2, 'B-ORG': 3, 'I-ORG': 4, 'B-LOC': 5, 'I-LOC': 6, 'B-MISC': 7,
                          'I-MISC': 8}
         self.id2label = {0: 'O', 1: 'B-PER', 2: 'I-PER', 3: 'B-ORG', 4: 'I-ORG', 5: 'B-LOC', 6: 'I-LOC', 7: 'B-MISC',
@@ -41,7 +37,8 @@ class NerMetrics:
                 outputs = self.model(inputs)
 
                 # Apply softmax to get probabilities and then argmax to get the predicted labels
-                preds = torch.argmax(outputs, dim=-1)
+                probs = F.softmax(outputs, dim=-1)
+                preds = torch.argmax(probs, dim=-1)
 
                 for i in range(len(true_labels)):
                     # Apply mask to remove -1 padded true labels and corresponding predictions

run_ner.py
@@ -1,15 +1,12 @@
 import argparse
 import json
-import random
 import torch
 from transformers import BertTokenizer
-from ner_dataloader import NerDataLoader, NerDataset, collate_fn
+from ner_dataloader import NerDataLoader, NerDataset
 from model_AGN import AGNModel, train_agn_model, test_agn_model
 from torch.utils.data import DataLoader
-import numpy as np
-import os
 import logging
-from utils import get_save_dir, set_seed
+from utils import get_save_dir, set_seed, collate_fn
 
 
 def check_device():

utils.py
@@ -81,7 +81,6 @@ class Swish(nn.Module):
         return x * torch.sigmoid(self.beta * x)
 
 
-
 def move_to_device(data, device):
     """Moves all data in the dictionary to the device"""
     for key, value in data.items():
@@ -110,6 +109,7 @@ def clean_str(string):
     string = re.sub(r"\?", " ? ", string)
     string = re.sub(r"\s{2,}", " ", string)
     return string.strip().lower()
+
 
 def get_save_dir(base_dir_name):
     # 1. Check if the result directory exists; if not, create it.
@@ -155,6 +155,7 @@ def read_json(file_path):
             data.append(record)
     return data
 
+
 def set_seed(seed):
     random.seed(seed)  # Python Random
     np.random.seed(seed)  # Numpy
@@ -164,3 +165,26 @@ def set_seed(seed):
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
+
+
+def collate_fn(batch):
+    batch_token_ids = [item['token_ids'] for item in batch]
+    batch_segment_ids = [item['segment_ids'] for item in batch]
+    batch_sf = [item['sf_vector'] for item in batch]
+    batch_label_ids = [item['label_ids'] for item in batch]
+
+    # Padding
+    batch_token_ids_padded = torch.nn.utils.rnn.pad_sequence(batch_token_ids, batch_first=True, padding_value=0)
+    attention_masks = torch.nn.utils.rnn.pad_sequence([torch.ones_like(ids) for ids in batch_token_ids],
+                                                      batch_first=True, padding_value=0)
+    batch_segment_ids_padded = torch.nn.utils.rnn.pad_sequence(batch_segment_ids, batch_first=True, padding_value=0)
+    batch_sf_padded = torch.nn.utils.rnn.pad_sequence(batch_sf, batch_first=True, padding_value=0)
+    batch_label_ids_padded = torch.nn.utils.rnn.pad_sequence(batch_label_ids, batch_first=True, padding_value=-100)
+
+    return {
+        'token_ids': batch_token_ids_padded,
+        'segment_ids': batch_segment_ids_padded,
+        'sf_vector': batch_sf_padded,
+        'attention_mask': attention_masks,
+        'label_ids': batch_label_ids_padded
+    }