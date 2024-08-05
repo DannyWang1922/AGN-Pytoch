3dcf2d2545893539c7cc849e5fbd68fc2fb6c0bc
DannyWang1922 <1922174653@qq.com>
2024-08-05 18:10:38 +0800

    dev: exp3.2: fix calc_metrics bug; add tqdm

--- ner_metrics.py
+++ ner_metrics.py
@@ -2,6 +2,8 @@ import logging
 from collections import defaultdict
 import torch
 from sklearn.preprocessing import MultiLabelBinarizer
+from tqdm import tqdm
+
 from utils import move_to_device, remove_cls_token
 from seqeval.metrics import accuracy_score, precision_score, recall_score, f1_score
 import torch.nn.functional as F
@@ -27,38 +29,11 @@ class NerMetrics:
         self.id2label = {0: 'O', 1: 'B-PER', 2: 'I-PER', 3: 'B-ORG', 4: 'I-ORG', 5: 'B-LOC', 6: 'I-LOC', 7: 'B-MISC',
                          8: 'I-MISC'}
 
-    # def calc_metrics(self):
-    #     self.model.eval()
-    #     y_pred, y_true = [], []
-    #     with torch.no_grad():
-    #         for data in self.eval_data_loader:
-    #             data = move_to_device(data, self.device)
-    #             inputs, true_labels = data, data["label_ids"]
-    #             outputs = self.model(inputs)
-    #
-    #             outputs, true_labels = remove_cls_token(outputs, true_labels)
-    #             attention_mask = (true_labels != -1).bool()
-    #             preds = self.model.predict(outputs, attention_mask.bool())
-    #
-    #             for i in range(len(true_labels)):
-    #                 # Apply mask to remove -1 padded true labels and corresponding predictions
-    #                 masked_true_labels = true_labels[i][true_labels[i] != -1].cpu().numpy().tolist()
-    #                 masked_preds = preds[i][true_labels[i] != -1].cpu().numpy().tolist()
-    #                 y_true.append([self.id2label[label] for label in masked_true_labels])
-    #                 y_pred.append([self.id2label[label] for label in masked_preds])
-    #
-    #     # 计算并打印评估指标
-    #     acc = round(accuracy_score(y_true, y_pred), 4)
-    #     precision = round(precision_score(y_true, y_pred), 4)
-    #     recall = round(recall_score(y_true, y_pred), 4)
-    #     f1 = round(f1_score(y_true, y_pred), 4)
-    #     return acc, precision, recall, f1
-
     def calc_metrics(self):
         self.model.eval()
         y_pred, y_true = [], []
         with torch.no_grad():
-            for data in self.eval_data_loader:
+            for data in tqdm(self.eval_data_loader, desc="Evaluating"):
                 data = move_to_device(data, self.device)
                 inputs, true_labels = data, data["label_ids"]
                 outputs = self.model(inputs)
@@ -66,15 +41,14 @@ class NerMetrics:
                 outputs, true_labels = remove_cls_token(outputs, true_labels)
                 attention_mask = (true_labels != -1).bool()
                 preds = self.model.predict(outputs, attention_mask.bool())
-                preds = [item for sublist in preds for item in sublist]  # change preds form 2d to 1d
-
-                true_label_mask = true_labels != -1  # remove -1 in true_labels
-                true_labels = true_labels[true_label_mask]
-                true_labels = true_labels.tolist()
 
                 for i in range(len(true_labels)):
-                    y_true.append([self.id2label[label] for label in true_labels])  # remove true label padding
-                    y_pred.append([self.id2label[label] for label in preds])
+                    # Apply mask to remove -1 padded true labels and corresponding predictions
+                    masked_true_labels = true_labels[i][true_labels[i] != -1].cpu().numpy().tolist()
+                    masked_preds = preds[i]
+                    y_true.append([self.id2label[label] for label in masked_true_labels])
+                    y_pred.append([self.id2label[label] for label in masked_preds])
+
 
         # 计算并打印评估指标
         acc = round(accuracy_score(y_true, y_pred), 4)
