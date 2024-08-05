babf4e3b43c793452c597ec5c4d92bb578c009ab
DannyWang1922 <1922174653@qq.com>
2024-08-05 18:20:28 +0800

    dev: exp3.1: update config

--- run_ner_experimets.py
+++ run_ner_experimets.py
@@ -13,4 +13,4 @@ seed_list = [37, 42, 52, 67, 80]
 for i in seed_list:
     print("=================================================================")
     print(f"Running experiment with seed={i}")
-    os.system(f"python run_ner.py --ae_epochs 100 --epochs 100 --config data/ner/conll2003_bert.json --random_seed {i}")
+    os.system(f"python run_ner.py --ae_epochs 1 --epochs 100 --config data/ner/conll2003_bert.json --random_seed {i}")
