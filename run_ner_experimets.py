15de81880250fd0a6aa5b5b807711fc3c979b381
DannyWang1922 <1922174653@qq.com>
2024-08-05 11:20:05 +0800

    dev: exp3.2

--- run_ner_experimets.py
+++ run_ner_experimets.py
@@ -8,9 +8,12 @@ import os
 #                "data/ner/conll2003_AGN_vae_softmax.json",
 #                "data/ner/conll2003_AGN_none_softmax.json"]
 
-
 seed_list = [37, 42, 52, 67, 80]
-for i in seed_list:
-    print("=================================================================")
-    print(f"Running experiment with seed={i}")
-    os.system(f"python run_ner.py --ae_epochs 100 --epochs 100 --config data/ner/conll2003_bert.json --random_seed {i}")
+batch_size_list = [16, 32]
+learning_rate_list = [5e-5, 3e-5, 2e-5]
+for seed in seed_list:
+    for bs in batch_size_list:
+        for lr in learning_rate_list:
+            print("=================================================================")
+            print(f"Running experiment with seed={seed}, bs={bs}, lr={lr}")
+            os.system(f"python run_ner.py --ae_epochs 1 --epochs 1 --config data/ner/conll2003_bert.json --batch_size {bs} --learning_rate {lr} --random_seed {seed}")
